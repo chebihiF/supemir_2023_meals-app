f864b7e9b4507795d9502cc995bbeb856b5e8657
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-04-12 15:06:52 +0000

    first commit

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,34 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+
+function MealsOverviewScreen({route}) {
+
+    const categoryId = route.params.categoryId;
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(categoryId) >= 0;
+    })
+
+    function renderMealsItem(itemData) {
+        return <MealItem title={itemData.item.title} />
+    }
+
+    return (
+        <View style= {styles.container}>
+            <FlatList 
+                data={displayedMeals} 
+                keyExtractor={(item)=> item.id}
+                renderItem={renderMealsItem}
+            />
+        </View>
+    );
+}
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16
+    }
+})
